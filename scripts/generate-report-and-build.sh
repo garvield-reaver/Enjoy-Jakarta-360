a08539e22bb0cf87f2a2c7e63407d7be3da78370
wildan <garviandiwildan@icloud.com>
2015-04-18 05:09:20 +0700

    setting up travis

---
+++ scripts/generate-report-and-build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#if [[ "$TRAVIS_BRANCH" != "master" ] || "$TRAVIS_PULL_REQUEST" == "true"]; then
+echo "Build on a branch other than master or from pull request."
+xctool -workspace ./Kurio/Moco.xcworkspace -scheme Moco -reporter json-compilation-database:compile_commands.json build -reporter pretty GCC_GENERATE_TEST_COVERAGE_FILES=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES
+
+oclint-json-compilation-database -e ./Kurio/Classes/Helper -e ./Kurio/Classes/Category -e ./Kurio/Pods -- -report-type text -o report.txt -max-priority-1=0 -max-priority-2=55 -max-priority-3=650 -rc LONG_LINE=150 -rc CYCLOMATIC_COMPLEXITY=100 -rc LONG_VARIABLE_NAME=50 -rc SHORT_VARIABLE_NAME=2 -disable-rule=UnusedMethodParameter -disable-rule=IvarAssignmentOutsideAccessorsOrInit
+#else
+#echo "Releasing build on a branch master."
+#xctool -workspace ./Kurio/Moco.xcworkspace -scheme Moco -configuration Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO -reporter pretty GCC_GENERATE_TEST_COVERAGE_FILES=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES
+#fi
